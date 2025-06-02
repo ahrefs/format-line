934569205b9bc1bd97748b2aa5ce39b0eff459f7
EmileTrotignon <emile.trotignon@gmail.com>
2025-06-02 18:02:30 +0200

    use cmdliner

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,40 @@
 open Ahrefs_whitespace
 
-let () =
-  let source = In_channel.(with_open_bin Sys.argv.(1) input_all) in
+let format ~source ~output =
   let lexbuf = Lexing.from_string source in
   let lexer = lexer lexbuf in
-  let output =
-    if Array.length Sys.argv > 2 then begin
-      let ch = Out_channel.open_bin Sys.argv.(2) in
-      output_string ch
-    end
-    else print_string
-  in
+  let output = output_string output in
   let first_token = lexer () in
   format ~output ~source ~lexer ~previous_token:first_token
+
+open Cmdliner
+
+let ( let* ) = Result.bind
+
+let cmd =
+  let term =
+    let open Term.Syntax in
+    let+ source =
+      Arg.(required & pos 0 (some file) None (info ~doc:"input file" []))
+    and+ output =
+      Arg.(value & opt (some file) None (info ~doc:"output" ["o"; "output"]))
+    and+ inline =
+      Arg.(value & flag (info ~doc:"inline formatting" ["i"; "inline"]))
+    and+ format = Term.const format in
+    let source_content = In_channel.(with_open_bin source input_all) in
+    let* output =
+      match (output, inline) with
+      | Some _, true ->
+          Error "--output and --inline cannot be specified at the same time."
+      | Some output, false ->
+          Ok (Out_channel.open_bin output)
+      | None, true ->
+          Ok (Out_channel.open_bin source)
+      | None, false ->
+          Ok stdout
+    in
+    Ok (format ~source:source_content ~output)
+  in
+  Cmd.v (Cmd.info "format-line") term
+
+let () = exit (Cmd.eval_result cmd)
